01e8f2e18d99dd9dd8875aba574db18fa8721dba
Adam Žluva <adam.zluva38@gmail.com>
2024-03-01 13:55:18 +0100

    Added Entity seed classes

---
+++ VUTIS2.DAL/Seeds/ActivitySeed.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VUTIS2.Common.Enums;
+using VUTIS2.DAL.Entities;
+
+namespace VUTIS2.DAL.Seeds
+{
+    public static class ActivitySeed
+    {
+        public static readonly ActivityEntity ExamIMA = new()
+        {
+            Id = Guid.Parse("2e5ddbaa-a6e1-466b-91c8-38804599e8aa"),
+            StartTime = new(2022, 1, 10, 8, 0, 0),
+            EndTime = new(2022, 1, 10, 10, 0, 0),
+            RoomName = "A1",
+            ActivityType = ActivityType.Exam,
+            Description = "Math analysis 1 exam",
+            SubjectId = SubjectSeed.SubjectIMA.Id,
+            Subject = SubjectSeed.SubjectIMA,
+            EvaluationId = EvaluationSeed.EvaluationIMAExam.Id,
+            Evaluation = EvaluationSeed.EvaluationIMAExam
+        };
+
+        public static void Seed(this ModelBuilder modelBuilder) =>
+            modelBuilder.Entity<SubjectEntity>().HasData(
+                ExamIMA with { Subject = null!, Evaluation = null! }
+            );
+    }
+}
---
+++ VUTIS2.DAL/Seeds/EvaluationSeed.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VUTIS2.DAL.Entities;
+
+namespace VUTIS2.DAL.Seeds
+{
+    public static class EvaluationSeed
+    {
+        public static readonly EvaluationEntity EvaluationIMAExam = new()
+        {
+            Id = Guid.Parse("92dd6d9d-bf28-4c90-9a23-c000b3829943"),
+            Points = 80,
+            Description = "Passed with flying colors",
+            ActivityId = ActivitySeed.ExamIMA.Id,
+            Activity = ActivitySeed.ExamIMA,
+            StudentId = StudentSeed.StudentB.Id,
+            Student = StudentSeed.StudentB
+        };
+
+        public static void Seed(this ModelBuilder modelBuilder) =>
+            modelBuilder.Entity<EvaluationEntity>().HasData(
+                EvaluationIMAExam with { Activity = null!, Student = null! }
+            );
+    }
+}
