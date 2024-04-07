7634d21a354ce3704fda13226989ea0e27cb0a29
Lukáš Píšek <lukin.pisek@email.cz>
2024-04-07 16:04:35 +0200

    review2

--- src/VUTIS2.DAL.Tests/DbContextActivityTests.cs
+++ src/VUTIS2.DAL.Tests/DbContextActivityTests.cs
@@ -1,8 +1,4 @@
-﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-//
-
-using VUTIS2.Common.Tests.Seeds;
+﻿using VUTIS2.Common.Tests.Seeds;
 using VUTIS2.DAL.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Internal;
