5b2155538108ad4404c046c5dc01474fa53a594c
Lukáš Píšek <lukin.pisek@email.cz>
2024-04-27 19:18:43 +0200

    feat: started work on student list view model

--- src/VUTIS2.App/Views/Student/StudentListViewModel.xaml.cs
+++ src/VUTIS2.App/Views/Student/StudentListViewModel.xaml.cs
@@ -1,6 +1,10 @@
-﻿namespace VUTIS2.App.Views;
+﻿using VUTIS2.App.ViewModels;
+namespace VUTIS2.App.Views.Student;
 
-public class StudentListViewModel
+public partial class StudentListView
 {
-
+    public StudentListView(StudentListViewModel viewModel) : base(viewModel)
+    {
+        InitializeComponent();
+    }
 }
