29cab1e867d017a71c669bcef2c59d2beb6c77f3
Lukáš Píšek <lukin.pisek@email.cz>
2024-05-01 14:49:23 +0200

    fix: doesnt work yet

---
+++ src/VUTIS2.App/Views/Activity/ActivityListViewModel.xaml.cs
@@ -0,0 +1,10 @@
+using VUTIS2.App.ViewModels;
+namespace VUTIS2.App.Views.Activity;
+
+public partial class ActivityEditView
+{
+    public ActivityEditView(ActivityEditViewModel viewModel) : base(viewModel)
+    {
+        InitializeComponent();
+    }
+}
