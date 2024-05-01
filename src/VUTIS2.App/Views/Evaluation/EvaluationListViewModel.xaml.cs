269e1d29b1c79bf55e6dc43471bc463db89f6496
Lukáš Píšek <lukin.pisek@email.cz>
2024-05-01 17:23:29 +0200

    feat: started work on evaluation list

---
+++ src/VUTIS2.App/Views/Evaluation/EvaluationListViewModel.xaml.cs
@@ -0,0 +1,10 @@
+using VUTIS2.App.ViewModels;
+namespace VUTIS2.App.Views.Evaluation;
+
+public partial class EvaluationListView
+{
+    public EvaluationListView(EvaluationListViewModel viewModel) : base(viewModel)
+    {
+        InitializeComponent();
+    }
+}
