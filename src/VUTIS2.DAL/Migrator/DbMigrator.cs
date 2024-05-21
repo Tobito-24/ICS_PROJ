70fc1f28f033e897cd138ac6d7683ffdb0e46930
Tobiáš Adamčík <tobias.adamcik@outlook.com>
2024-05-21 11:43:16 +0200

    Revert "idk humr"
    This reverts commit ef6674089d44becfbc731e91bc72df6c24ae7012.

--- src/VUTIS2.DAL/Migrator/DbMigrator.cs
+++ src/VUTIS2.DAL/Migrator/DbMigrator.cs
@@ -16,6 +16,10 @@ public class DbMigrator(IDbContextFactory<SchoolDbContext> dbContextFactory, DAL
         {
             await dbContext.Database.EnsureDeletedAsync(cancellationToken);
         }
+
+        // Ensures that database is created applying the latest state
+        // Application of migration later on may fail
+        // If you want to use migrations, you should create database by calling  dbContext.Database.MigrateAsync(cancellationToken) instead
         await dbContext.Database.EnsureCreatedAsync(cancellationToken);
     }
 }
